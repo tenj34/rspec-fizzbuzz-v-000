c5e5f986c6f67253c480f66170534a91aec1eaf5
Juan Tenezaca <tenj34@gmail.com>
2018-06-10 16:49:05 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz (int)
-if int % 3 == 0
-  puts "Fizz"
-end
-elsif int % 5 == 0
-  "Buzz"
-end
-elsif int % 3 == 0 && int % 5 == 0
-  "fizzbuzz"
-end
+  if int % 3 == 0
+    puts "Fizz"
+  end
+  elsif int % 5 == 0
+    "Buzz"
+  end
+  elsif int % 3 == 0 && int % 5 == 0
+    "fizzbuzz"
+  end
 end
